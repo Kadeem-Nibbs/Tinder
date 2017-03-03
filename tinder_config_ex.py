8d4c7857dbb299e63192651694edec2912a53666
Fabien Bessez <fabien.bessez@gmail.com>
2017-03-03 14:37:10 -0500

    config help

--- tinder_config_ex.py
+++ tinder_config_ex.py
@@ -1,6 +1,8 @@
 import fb_auth_token 
 
-fb_auth_token = fb_auth_token.get_fb_access_token(<fbusername>, <fbpassword>)
+fb_username = <fb_username>
+fb_password = <fb_password>
+fb_access_token = fb_auth_token.get_fb_access_token(fb_username, fb_password)
 fb_user_id = <id>
 
 
